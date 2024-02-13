6cf7a86ecebe502ddc08507ea76a5bf2008807a2
Fel-ly <kiokofpm@gmail.com>
2024-02-12 22:22:25 -0800

    update

--- src/hello.java
+++ src/hello.java
@@ -3,20 +3,21 @@ import java.util.Scanner;
 
 public class hello {
     public static void main (String[] args){
+        System.out.println("Hello World!");
 
 //        String name = JOptionPane.showInputDialog(null,"Enter your name: ");
 //        System.out.println("Hello  " +  name + ". Nice to meet you!");
 
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Hellooooo!!!");
-        System.out.println("Enter first number: ");
-        int num1 = scanner.nextInt();
-        System.out.println("Enter second number: ");
-        int num2 = scanner.nextInt();
-        int sum = num1 + num2;
-        System.out.println(" The sum is " + sum);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Hellooooo!!!");
+//        System.out.println("Enter first number: ");
+//        int num1 = scanner.nextInt();
+//        System.out.println("Enter second number: ");
+//        int num2 = scanner.nextInt();
+//        int sum = num1 + num2;
+//        System.out.println(" The sum is " + sum);
 
     }
 }
